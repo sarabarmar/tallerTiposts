8b98b8de8bcc74d1449bdbcf8e3e0ce55e068777
Barrera <barrerasara2008@gmail.com>
2025-10-16 15:52:47 -0500

    Segundo Commit

--- src/Tarea.ts
+++ src/Tarea.ts
@@ -1,6 +1,21 @@
-interface Tarea{
-    id : number;
-    descripcion : string;
-    completada : boolean;
-    estado : "Pendiente" | "Progreso";
+interface Tarea {
+    id: number;
+    descripcion: string;
+    completada: boolean;
+    estado: "pendiente" | "en_progreso" | "finalizada";
 }
+
+function Tareas(tareas: Tarea[]) {
+    return tareas.filter(
+        tarea => tarea.estado === "pendiente" || tarea.estado === "en_progreso"
+    );
+}
+const listaTareas: Tarea[] = [
+    { id: 1, descripcion: "Estudiar TypeScript", completada: true, estado: "finalizada"},
+    { id: 2, descripcion: "Estudiar Java", completada: false, estado: "en_progreso"},
+    { id: 3, descripcion: "Estudiar C#", completada: false, estado: "pendiente"},
+]
+
+const tareasPendientes = Tareas (listaTareas)
+    console.log(tareasPendientes);
+    
